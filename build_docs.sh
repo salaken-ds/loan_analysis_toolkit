5b15666a3b1260400918bf5a0ec87aafd16007b6
Salaken Syed <salaken.buet.eee@gmail.com>
2025-09-24 01:23:22 +1000

    Updated docs

--- build_docs.sh
+++ build_docs.sh
@@ -1,6 +1,7 @@
 jupyter-book build notebook/ --builder html --path-output docs
 cp -r docs/_build/html/* docs/
 rm -rf docs/_build/
+touch docs/.nojekyll
 git add .
 git commit -m "Updated docs"
 git push origin master
